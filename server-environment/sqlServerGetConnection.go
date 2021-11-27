4528cd84c9ac97fc1fff486cf7d99acedc9ee258
Yulian Adolfo Rojas <Yulianrojas2000@gmail.com>
2021-11-27 17:52:53 -0500

    code to establish connection to sql server

---
+++ server-environment/sqlServerGetConnection.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/denisenkom/go-mssqldb"
+)
+
+const (
+	serverSql    = "192.168.1.198\\SQLEXPRESS"
+	portSql      = 1443
+	userSql      = "consentimientos"
+	passwordSql  = "patientClinicH.21*"
+	databaseName = "PRUEBA-5"
+)
+
+func sqlServerGetConnection() *sql.DB {
+	// creating the connection to sql server
+	connectionQueryParams := fmt.Sprintf("server=%s;user id=%s;password=%s;port=%d;database=%s;", serverSql, userSql, passwordSql, portSql, databaseName)
+	// creating the connection pool
+	databaseConnection, err := sql.Open("sqlserver", connectionQueryParams)
+	if err != nil {
+		fmt.Println("Error opening the connection to sql server: " + err.Error())
+	}
+	ctx := context.Background()
+	// Ping database to see if it's still alive.
+	// Important for handling network issues and long queries.
+	err = databaseConnection.PingContext(ctx)
+	if err != nil {
+		log.Fatal("Error pinging database: " + err.Error())
+	}
+	return databaseConnection
+}
