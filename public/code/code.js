a2646efe4ee18da064542104ce600c0637430aaf
Yulian Adolfo Rojas <Yulianrojas2000@gmail.com>
2021-12-02 17:30:42 -0500

    code to insert patient into remote table, get report

--- public/code/code.js
+++ public/code/code.js
@@ -63,6 +63,8 @@ generateReportButton.onclick = () => {
 
     item1.innerHTML = "Fecha de historia"
     item2.innerHTML = "Fecha de registro"
+    item1.value = 1
+    item2.value = 0
     select.appendChild(item1)
     select.appendChild(item2)
     select.classList.add("select-of-report")
@@ -83,24 +85,31 @@ generateReportButton.onclick = () => {
     button.onclick = async () => {
         var dateStart = date1.value
         var dateEnd = date2.value
-        if(dateStart != "" && dateEnd != "") {
+        if (dateStart != "" && dateEnd != "") {
             var checkPatientError = checkPatientErrors.children[0].checked
+            if (checkPatientError) {
+                checkPatientError = 1
+            } else {
+                checkPatientError = 0
+            }
             var checkView = checkOnlyView.children[0].checked
-            var query = "?date-start=" + dateStart + "&date-end=" + dateEnd + "&check-only-p-errors=" + checkPatientError + "&check-only-view=" + checkView  + "&gen-by=" + select.value
-            var state = await new Promise((resolved, rejected)=> {
-                fetch("/get-information-from-patient" + query,{
-                    method:"get"  
+            var query = "?date-start=" + dateStart + "&date-end=" + dateEnd + "&check-only-p-errors=" + checkPatientError + "&check-only-view=" + checkView + "&gen-by=" + select.value
+            onprogressRequest()
+            var state = await new Promise((resolved, rejected) => {
+                fetch("/get-information-from-patient" + query, {
+                    method: "get"
                 })
-                .then(data => data.json())
-                .then(data => resolved(data))
-                .catch(error => rejected(error))
+                    .then(data => data.json())
+                    .then(data => resolved(data))
+                    .catch(error => rejected(error))
             })
-            console.log(state)
-        }else {
+            tableInfo(state)
+            removeLastElement()
+        } else {
             stateProcessAlert("fa-info-circle", "Faltan campos por llenar, por favor verifique", "orange")
         }
     }
-    
+
     function createCheckboxes(idCheckbox, contentLabel) {
         var spanCheckbox = document.createElement("span")
         var checkbox = document.createElement("input")
@@ -117,7 +126,7 @@ generateReportButton.onclick = () => {
         spanCheckbox.appendChild(label)
         return spanCheckbox
     }
-}  
+}
 generateReportButton.click()
 function deleteActualWin(app, div) {
     app.removeChild(div)
@@ -125,20 +134,20 @@ function deleteActualWin(app, div) {
 }
 //const IP_SERVER = "http://192.168.11.105:8005/"
 IdPatientBox.onchange = async () => {
-    var id = parseInt(IdPatientBox.value)   
+    var id = parseInt(IdPatientBox.value)
     if (!isNaN(id)) {
         var stateMessage = " | consultando..."
         IdPatientBox.disabled = true
         IdPatientBox.style.backgroundColor = "rgba(1, 172, 240)"
         IdPatientBox.style.color = "white"
         IdPatientBox.value = IdPatientBox.value + stateMessage
-        var getInfoPatient = await new Promise((resolved, rejected)=> {
+        var getInfoPatient = await new Promise((resolved, rejected) => {
             fetch("/get-data-patient?id-patient=" + id, {
-                method:"get"
+                method: "get"
             })
-            .then(resp => resp.json())
-            .then(data => resolved(data))
-            .then(error => rejected(error))
+                .then(resp => resp.json())
+                .then(data => resolved(data))
+                .then(error => rejected(error))
         })
         stateMessage = ""
         IdPatientBox.disabled = false
@@ -149,15 +158,15 @@ IdPatientBox.onchange = async () => {
             boxNames.value = getInfoPatient.Names
             boxLastnames.value = getInfoPatient.Lastnames
             typeIdPatient.value = getInfoPatient.TypId
-        }else {
+        } else {
             boxNames.value = ""
             boxLastnames.value = ""
-            stateProcessAlert("fa-address-book", "Sin registros en nuestro sistema interno", "rgb(243, 98, 1)")   
+            stateProcessAlert("fa-address-book", "Sin registros en nuestro sistema interno", "rgb(243, 98, 1)")
         }
     }
 }
 function boxesForm() {
-    
+
 }
 buttonSender()
 function getDiv() {
@@ -173,14 +182,14 @@ function getAlertDiv() {
 function buttonSender() {
     buttonSubmit.onclick = () => {
         // getting values from boxes in form
-        var documentId = IdPatientBox.value 
+        var documentId = IdPatientBox.value
         var typeId = typeIdPatient.value
         var names = boxNames.value
         var lastnames = boxLastnames.value
         var dateHc = dateRegistry.value
         var patientErrors = checkboxError.value
-    
-        switch(typeId) {
+
+        switch (typeId) {
             case "0":
                 typeId = "CC"
                 break;
@@ -218,11 +227,11 @@ function buttonSender() {
                 typeId = ""
                 break;
         }
-        if(documentId != "" && documentId.length > 5 && names != "" && lastnames != "" && dateHc != "" && typeId != "") {
+        if (documentId != "" && documentId.length > 5 && names != "" && lastnames != "" && dateHc != "" && typeId != "") {
             // checking if the patient has errors
-            if(checkboxError.checked) {
+            if (checkboxError.checked) {
                 patientErrors = true
-            }else {
+            } else {
                 patientErrors = false
             }
             // data in object
@@ -236,35 +245,35 @@ function buttonSender() {
             }
             // converting to json
             var sendRecord = JSON.stringify(recordDataPatient)
-    
+
             async function sendRecordToServer() {
                 // setting the loading progress
                 onprogressRequest()
                 // sending the data
                 var stateRecord = await new Promise((recorded, rejected) => {
-                    fetch("/record-patient", 
-                    {
-                        method: "post",
-                        headers: {
-                            "content-type":"application/json"
-                        },
-                        body: sendRecord
-                    })
-                    .then(resp => resp.json())
-                    .then(data => recorded(data))
-                    .catch(error => rejected(error))
+                    fetch("/record-patient",
+                        {
+                            method: "post",
+                            headers: {
+                                "content-type": "application/json"
+                            },
+                            body: sendRecord
+                        })
+                        .then(resp => resp.json())
+                        .then(data => recorded(data))
+                        .catch(error => rejected(error))
                 })
                 stateRecord = stateRecord.ContenMessage
                 if (stateRecord.includes("Error 1062") || stateRecord.includes("Duplicate entry")) {
-                    stateProcessAlert("fa-user-times","Usuario existente, se ha denegado el registro", "red")
-                }else if(stateRecord.includes("dial tcp: i/o timeout")) {
-                    stateProcessAlert("fa-user-times","Lo sentimos, inténtelo nuevamente (dial/tcp)", "red")
-                }else {
-                    if(stateRecord == "successfull") {
+                    stateProcessAlert("fa-user-times", "Usuario existente, se ha denegado el registro", "red")
+                } else if (stateRecord.includes("dial tcp: i/o timeout")) {
+                    stateProcessAlert("fa-user-times", "Lo sentimos, inténtelo nuevamente (dial/tcp)", "red")
+                } else {
+                    if (stateRecord == "successfull") {
                         stateProcessAlert("fa-user-check", "Registro éxitoso", "limegreen")
                     }
                 }
-                IdPatientBox.value  = ""
+                IdPatientBox.value = ""
                 boxNames.value = ""
                 boxLastnames.value = ""
                 checkboxError.value = ""
@@ -272,36 +281,36 @@ function buttonSender() {
                 removeLastElement()
             }
             sendRecordToServer()
-        }else {
+        } else {
             stateProcessAlert("fa-info-circle", "Faltan campos por llenar, por favor verifique", "orange")
         }
     }
 }
 function stateProcessAlert(iconClass, message, backgroundColor) {
-        var successfull = getDiv()
-        var messageP =  document.createElement("p")
-        var icon = document.createElement("i")
-        icon.classList.add("fas", iconClass)
-        messageP.innerHTML =  message
-        successfull.style.backgroundColor = backgroundColor
-        successfull.appendChild(icon)
-        successfull.appendChild(messageP)
-        successfull.classList.add("alert-state", "transition-show")
-        document.body.appendChild(successfull)
+    var successfull = getDiv()
+    var messageP = document.createElement("p")
+    var icon = document.createElement("i")
+    icon.classList.add("fas", iconClass)
+    messageP.innerHTML = message
+    successfull.style.backgroundColor = backgroundColor
+    successfull.appendChild(icon)
+    successfull.appendChild(messageP)
+    successfull.classList.add("alert-state", "transition-show")
+    document.body.appendChild(successfull)
+    setTimeout(() => {
+        successfull.classList.remove("transition-show")
+        successfull.classList.add("transition-hide")
         setTimeout(() => {
-            successfull.classList.remove("transition-show")
-            successfull.classList.add("transition-hide")
+            successfull.classList.remove("transition-hide")
+            successfull.style.transform = "translateX(110%)"
+            successfull.style.transition = "1s"
             setTimeout(() => {
-                successfull.classList.remove("transition-hide") 
-                successfull.style.transform = "translateX(110%)"
-                successfull.style.transition = "1s"
-                setTimeout(() => {
-                    document.body.removeChild(successfull)
-                }, 1000);
-            }, 2000);
-        }, 100);
+                document.body.removeChild(successfull)
+            }, 1000);
+        }, 2000);
+    }, 100);
 }
-function setFailProcessAlert()  {
+function setFailProcessAlert() {
     var fail = getDiv()
     fail.innerHTML += "<i class='fas fa-exclamation-triangle'></i>El paciente ingresado ya existe. Se ha denegado el registro"
     fail.style.backgroundColor = "red"
@@ -319,7 +328,7 @@ buttonOpenMenu.onclick = () => {
 }
 function onprogressRequest() {
     iconHeader("none")
-    var onprogress = getAlertDiv()   
+    var onprogress = getAlertDiv()
     var blockMessage = getAlertDiv()
     var iconLoad = getAlertDiv()
 
@@ -333,7 +342,7 @@ function onprogressRequest() {
     onprogress.style.justifyContent = "center"
 
     blockMessage.style.width = "100px"
-    blockMessage.style.height ="60px"
+    blockMessage.style.height = "60px"
     blockMessage.style.backgroundColor = "#00adef"
     blockMessage.style.marginTop = "-100px"
     blockMessage.style.borderRadius = "10px"
@@ -350,19 +359,76 @@ function onprogressRequest() {
     app.appendChild(onprogress)
 }
 function removeLastElement() {
-   app.removeChild(app.lastElementChild)
+    app.removeChild(app.lastElementChild)
 }
 function iconHeader(state) {
     var icon = app.getElementsByTagName('header')
     icon[1].children[0].style.display = state
 }
 function convertNameTo(string) {
-    if(string != "") {
+    if (string != "") {
         var str1 = string[0]
         var str2 = str1 + string.substring(1, string.length).toLowerCase()
         string = str2
         return string
-    }else {
+    } else {
         return ""
     }
 }
+function tableInfo(contentQuery) {
+    if (contentQuery != null) {
+        var table = document.getElementById("table-info-view-patient")
+        var closeInfo = document.getElementById("close-modal-info")
+        for (var i = 0; i < contentQuery.length; i++) {
+            var tr = document.createElement("tr")
+            var data = JSON.parse(contentQuery[i])
+            for (var j = 0; j < 7; j++) {
+                var td = document.createElement("td")
+                switch (j) {
+                    case 0:
+                        td.innerHTML = data.IdPatient
+                        break;
+                    case 1:
+                        td.innerHTML = data.TypeId
+                        break;
+                    case 2:
+                        td.innerHTML = data.DateClinicHistory
+                        break;
+                    case 3:
+                        td.innerHTML = data.ActualDateRegistry
+                        break;
+                    case 4:
+                        td.innerHTML = data.PatientNames
+                        break;
+                    case 5:
+                        td.innerHTML = data.PatientLastnames
+                        break;
+                    case 6:
+                        td.innerHTML = data.HasError
+                        break;
+                }
+                tr.appendChild(td)
+            }
+            table.appendChild(tr)
+        }
+        showInfoModal()
+        closeInfo.onclick = () => {
+            closeInfoModal()
+        }
+    }else {
+        stateProcessAlert("fa-address-book", "No se han encontrado registros", "rgb(243, 98, 1)")   
+    }
+}
+function closeInfoModal() {
+    document.getElementById("information-view-patient").style.display = "none"
+    var tbody = document.getElementById("table-info-view-patient")
+    while(tbody.children.length > 1) {
+        tbody.removeChild(tbody.lastElementChild)
+    }
+}
+function showInfoModal() {
+    document.getElementById("information-view-patient").style.display = "block"
+}
+closeInfoModal()
+
+
