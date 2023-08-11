013cf0fbc5c093012587b262338bf0429dc430c2
Yulian Rojas <yulianrojas2000@gmail.com>
2023-08-11 18:47:08 -0500

    now the file will read the parameters from the json file

--- server-environment/connectMysql.go
+++ server-environment/connectMysql.go
@@ -4,19 +4,13 @@ import (
 	"database/sql"
 	"fmt"
 
+	"Yuls/readerparams"
+
 	_ "github.com/go-sql-driver/mysql"
 )
 
-const (
-	username     = "ipsucjyl_userRegisterCH"
-	password     = "RegThisPatient.21Yuls*"
-	port         = "3306"
-	host         = "cp-31.webhostbox.net"
-	typeConn     = "tcp"
-	databaseConn = "ipsucjyl_patient_clinic_history"
-)
-
 func getConnectionDB() *sql.DB {
+	username, password, typeConn, host, port, databaseConn := readerparams.ReadConnectionMySqlParameters()
 	connection, err := sql.Open("mysql", username+":"+password+"@"+typeConn+"("+host+":"+port+")/"+databaseConn)
 	if err != nil {
 		fmt.Println("Error trying connection to remote database: " + err.Error())
