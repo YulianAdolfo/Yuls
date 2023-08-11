1c87ec4c4d170f63707315e9b08d24f18586ee65
Yulian Rojas <yulianrojas2000@gmail.com>
2023-08-11 18:12:30 -0500

    a file to save parameters for connection app

---
+++ readerparams/reader.go
@@ -0,0 +1,42 @@
+package readerparams
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+)
+
+type sqlConnection struct {
+	Server   string
+	Port     string
+	Username string
+	Password string
+	Database string
+}
+
+func initReader() (*os.File, error) {
+	file, err := os.Open("../PARAMETERS/APP_PARAMETERS.json")
+	return file, err
+}
+func ReadConnectionSqlParameters() (string, string, string, string, string) {
+	file, err := initReader()
+	if err != nil {
+		fmt.Println("¡Error al leer parametros! ", err)
+	}
+	data := readParameters(file)
+	server := data["SqlHosvitalConnection"].(map[string]interface{})["Server"].(string)
+	username := data["SqlHosvitalConnection"].(map[string]interface{})["Username"].(string)
+	password := data["SqlHosvitalConnection"].(map[string]interface{})["Password"].(string)
+	port := data["SqlHosvitalConnection"].(map[string]interface{})["Port"].(string)
+	database := data["SqlHosvitalConnection"].(map[string]interface{})["Database"].(string)
+	defer file.Close()
+	return server, username, password, port, database
+
+}
+func readParameters(file *os.File) map[string]interface{} {
+	valuesinJson, _ := io.ReadAll(file)
+	var valuesFromJson map[string]interface{}
+	json.Unmarshal([]byte(valuesinJson), &valuesFromJson)
+	return valuesFromJson
+}
